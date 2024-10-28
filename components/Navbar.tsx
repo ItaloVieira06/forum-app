da65a9afad44fa380eee20e8ce62fdb80c98fe15
Ítalo do Espírito Santo Vieira <129171110+ItaloVieira06@users.noreply.github.com>
2024-10-28 16:20:20 -0300

    fixes and updations

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,15 +6,14 @@ export default function LoginPage() {
   const router = useRouter();
 
   return (
-   <>
     <nav className={styles.navbar}>
       <div className={styles.logo}>
         <Link href="/">
-         Logo
+          Logo
         </Link>
       </div>
       <ul className={styles.navList}>
-        <li className={router.pathname === '/' ? styles.active : ''}>
+        <li className={`${router.pathname === '/' ? styles.active : ''} ${styles.bordered}`}>
           <Link href="/">Home</Link>
         </li>
         <li className={router.pathname === '/faq' ? styles.active : ''}>
@@ -23,15 +22,13 @@ export default function LoginPage() {
         <li className={router.pathname === '/questions' ? styles.active : ''}>
           <Link href="/questions">Questions</Link>
         </li>
-        <li className={router.pathname === '/login' ? styles.active : ''}>
+        <li className={`${router.pathname === '/login' ? styles.active : ''} ${styles.bordered}`}>
           <Link href="/login">Login</Link>
         </li>
-        <li className={router.pathname === '/register' ? styles.active : ''}>
+        <li className={`${router.pathname === '/login' ? styles.active : ''} ${styles.bordered2}`}>
           <Link href="/register">Register</Link>
         </li>
       </ul>
     </nav>
-    <div className={styles.separator} />
-   </>
   );
 }
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,5 +1,7 @@
 'use client'
 import { useState } from 'react';
+import React from 'react';
+import styles from '../styles/Login.module.css';
 
 export default function LoginPage() {
 
@@ -8,42 +10,43 @@ export default function LoginPage() {
   
     const handleSubmit = (e) => {
       e.preventDefault();
-      // Aqui você pode adicionar a lógica para autenticação
       console.log('Email:', email);
       console.log('Password:', password);
     }
     
 
   return (
-    <div style={{ maxWidth: '400px', margin: '50px auto', padding: '20px', border: '1px solid #ccc', borderRadius: '5px' }}>
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
-        <div style={{ marginBottom: '15px' }}>
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            style={{ width: '100%', padding: '2px', marginTop: '10px' }}
-          />
-        </div>
-        <div style={{ marginBottom: '15px' }}>
-          <label htmlFor="password">Senha:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-            style={{ width: '100%', padding: '2px', marginTop: '10px' }}
-          />
-        </div>
-        <button type="submit" style={{ padding: '10px 15px', backgroundColor: '#0070f3', color: 'white', border: 'none', borderRadius: '5px' }}>
-          Logar
-        </button>
-      </form>
+    <div className={styles.pageContainer}>
+      <h1 className={styles.title}>Login</h1> 
+      <div className={styles.loginBox}>
+       <form onSubmit={handleSubmit}>
+         <div style={{ marginBottom: '15px' }}>
+           <label htmlFor="email">Email:</label>
+           <input
+             type="email"
+             id="email"
+             value={email}
+             onChange={(e) => setEmail(e.target.value)}
+             required
+             style={{ width: '100%', padding: '2px', marginTop: '10px' }}
+           />
+         </div>
+         <div style={{ marginBottom: '15px' }}>
+           <label htmlFor="password">Senha:</label>
+           <input
+             type="password"
+             id="password"
+             value={password}
+             onChange={(e) => setPassword(e.target.value)}
+             required
+             style={{ width: '100%', padding: '2px', marginTop: '10px' }}
+           />
+         </div>
+         <button type="submit" style={{ padding: '10px 15px', backgroundColor: '#0070f3', color: 'white', border: 'none', borderRadius: '5px' }}>
+           Logar
+         </button>
+       </form>
+     </div>
     </div>
   );
 }
