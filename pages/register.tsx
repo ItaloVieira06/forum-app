ad85e0d99f2aa6a6db017a6e6301e5d1af849837
Ítalo do Espírito Santo Vieira <129171110+ItaloVieira06@users.noreply.github.com>
2024-10-28 17:02:44 -0300

    Update register.tsx

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,58 +1,65 @@
 'use client'
 import { useState } from 'react';
+import React from 'react';
+import styles from '../styles/Login.module.css';
 
 export default function LoginPage() {
 
+    const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
   
     const handleSubmit = (e) => {
       e.preventDefault();
-      // Aqui você pode adicionar a lógica para autenticação
+      console.log('Name:', name)
       console.log('Email:', email);
       console.log('Password:', password);
     }
     
 
   return (
-    <div style={{ maxWidth: '400px', margin: '50px auto', padding: '20px', border: '1px solid #ccc', borderRadius: '5px' }}>
-      <h1>Register</h1>
-      <form onSubmit={handleSubmit}>
-      <div style={{ marginBottom: '15px' }}>
-          <label htmlFor="email">Name:</label>
-          <input
-            type="text"
-            id="text"
-            required
-            style={{ width: '100%', padding: '2px', marginTop: '10px' }}
-          />
-        </div>
-        <div style={{ marginBottom: '15px' }}>
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            style={{ width: '100%', padding: '2px', marginTop: '10px' }}
-          />
-        </div>
-        <div style={{ marginBottom: '15px' }}>
-          <label htmlFor="password">Senha:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-            style={{ width: '100%', padding: '2px', marginTop: '10px' }}
-          />
-        </div>
-        <button type="submit" style={{ padding: '10px 15px', backgroundColor: '#0070f3', color: 'white', border: 'none', borderRadius: '5px' }}>
-          Registrar
-        </button>
-      </form>
+    <div className={styles.pageContainer}>
+      <h1 className={styles.title}>Register</h1> 
+      <div className={styles.loginBox}>
+       <form onSubmit={handleSubmit}>
+       <div style={{ marginBottom: '15px' }}>
+           <label htmlFor="email">Name:</label>
+           <input
+             type="text"
+             id="name"
+             value={name}
+             onChange={(e) => setName(e.target.value)}
+             required
+             style={{ width: '100%', padding: '2px', marginTop: '10px' }}
+           />
+         </div>
+         <div style={{ marginBottom: '15px' }}>
+           <label htmlFor="email">Email:</label>
+           <input
+             type="email"
+             id="email"
+             value={email}
+             onChange={(e) => setEmail(e.target.value)}
+             required
+             style={{ width: '100%', padding: '2px', marginTop: '10px' }}
+           />
+         </div>
+         <div style={{ marginBottom: '15px' }}>
+           <label htmlFor="password">Senha:</label>
+           <input
+             type="password"
+             id="password"
+             value={password}
+             onChange={(e) => setPassword(e.target.value)}
+             required
+             style={{ width: '100%', padding: '2px', marginTop: '10px' }}
+           />
+         </div>
+         <button type="submit" className={styles.loginButton}>
+           Register
+         </button>
+       </form>
+     </div>
     </div>
   );
 }
